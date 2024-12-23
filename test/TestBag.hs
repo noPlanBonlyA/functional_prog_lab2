b67f35509d1c7397adab9ff8318564f83cdaf69e
noPlanBonlyA <andrey.spb521@gmail.com>
2024-12-23 17:58:37 +0300

    not test

--- test/TestBag.hs
+++ test/TestBag.hs
@@ -50,7 +50,7 @@ testRemoveWithCount = do
 testMapInc :: Assertion
 testMapInc = do
   let bag = insert 1 $ insert 1 $ insert 2 createBag
-  let bag' = mapBag (+1) bag
+  let bag' = mapBag (+ 1) bag
   find 2 bag' @?= Just 2
   find 3 bag' @?= Just 1
 
@@ -58,7 +58,7 @@ testFilter :: Assertion
 testFilter = do
   let bag = insert 1 $ insert 1 $ insert 2 createBag
   putStrLn $ "Original bag: " ++ show bag
-  let bag' = filterBag (\count -> count > 1) bag
+  let bag' = filterBag (> 1) bag
   putStrLn $ "Filtered bag: " ++ show bag'
   find 1 bag' @?= Just 2
   find 2 bag' @?= Nothing
@@ -68,4 +68,3 @@ testFold = do
   let bag = insert 1 $ insert 1 $ insert 2 createBag
   let sum = foldlBag (\(_, count) acc -> acc + count) 0 bag
   sum @?= 3
-  
